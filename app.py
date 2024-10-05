178d496c56f829076e106920e0e3bc13282d04fc
Draksham Tharun <123640464+draksham@users.noreply.github.com>
2024-10-06 00:17:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ def main():
         st.sidebar.markdown(f"""
             <h3 align="left">Connect with me:</h3>
             <p align="left">
-                <a href="https://www.linkedin.com/in/draksham-tharun" target="_blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="LinkedIn" height="30" width="40" /></a>
+                <a href="https://www.linkedin.com/in/tharun-draksham-361285257/" target="_blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="LinkedIn" height="30" width="40" /></a>
                 <a href="https://www.kaggle.com/draksham" target="_blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/kaggle.svg" alt="Kaggle" height="30" width="40" /></a>
                 <a href="https://github.com/draksham-tharun" target="_blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/github.svg" alt="Github" height="30" width="40" /></a>
                 <a href="https://leetcode.com/u/lcs2022015/" target="_blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/leet-code.svg" alt="LeetCode" height="30" width="40" /></a>
